69ef860d329937cfc9e471eb1fc361b8fac8613d
Freddie <fredjlawson@gmail.com>
2018-09-05 16:43:22 +0100

    adds temperature reset and minumum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,30 +1,46 @@
 describe('Thermostat', function() {
-    let thermostat;
+  let thermostat;
+  
+  beforeEach(function() {
+      thermostat = new Thermostat();
+  })
     
-    beforeEach(function() {
-        thermostat = new Thermostat();
-    })
-    
-    
-    describe('#temperature', function() {
-      
-        it('returns the start temperature', function() {
-          expect(thermostat.temperature).toEqual(20)
-        });
+  describe('#temperature', function() {
+    it('returns the start temperature', function() {
+      expect(thermostat.temperature).toEqual(20)
     });
+  });
 
-   describe('#increase', function() {
-       it('increases the temperature', function() {
-           thermostat.increase();
-           expect(thermostat.temperature).toEqual(21)
-       });
-   });
-    
-    describe('#decrease', function() {
-       it('dereases the temperature', function() {
-           thermostat.decrease();
-           expect(thermostat.temperature).toEqual(19)
-       });
-   });
+  describe('#increase', function() {
+    it('increases the temperature', function() {
+        thermostat.increase();
+        expect(thermostat.temperature).toEqual(21)
+    });
+  });
+  
+  describe('#decrease', function() {
+    it('dereases the temperature', function() {
+        thermostat.decrease();
+        expect(thermostat.temperature).toEqual(19)
+    });
+    it('won\'t decrease temperature below the minimum', function() {
+      while (thermostat.temperature > 10) {
+        thermostat.decrease();
+    }
+      thermostat.decrease()
+      thermostat.decrease()
+      expect(thermostat.temperature).toEqual(10);
+    });
+  });
+
+  describe('#reset', function() {
+    it('resets the temperature', function() {
+      thermostat.increase();
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,19 @@
 function Thermostat(temp = 20) {
   this.temperature = temp;
+  this.minimumTemperature = 10;
 }
 
 Thermostat.prototype.increase = function() {
-    this.temperature += 1;
+  this.temperature += 1;
 };
 
 Thermostat.prototype.decrease = function() {
+  if (this.temperature > this.minimumTemperature) {
     this.temperature -= 1;
+  }
+
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
 };
