8d351600c624f69abce907f7e7453a168cd627d5
Aneel Marshall <aneel.marshall@outlook.com>
2018-09-07 14:35:34 +0100

    add click button events to correspond to thermostat.js

---
+++ src/script.js
@@ -0,0 +1,24 @@
+const t = new Thermostat()
+
+$(document).ready(function() {
+    $('#display').text(t.getTemperature());
+    $('#increaseTemp').click(function() {
+        t.increase();
+    });
+    $('#decreaseTemp').click(function() {
+        t.decrease();
+    });
+    $('#reset').click(function() {
+        t.reset();
+    });
+    $('#switchPower').click(function() {
+        t.powerSavingSwitch();
+    });
+    $('button').click(function() {
+        $('#display').text(t.getTemperature());
+    });
+    
+    
+    
+    
+});
